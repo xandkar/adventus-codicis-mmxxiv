f1572e546c6437f63f6d2dfe4a606a585ca7972e
Siraaj Khandkar <siraaj@khandkar.net>
2024-12-04 18:14:58 -0500

    rectifica formam

--- dies_n.rs
+++ dies_n.rs
@@ -1,30 +1,30 @@
 use std::io::BufRead;
 
-use anyhow::{anyhow, Result};
+use anyhow::anyhow;
 
 pub struct Data {}
 
 impl Data {
-    pub fn load(input: &std::path::Path) -> Result<Self> {
+    pub fn load(input: &std::path::Path) -> anyhow::Result<Self> {
         let file = std::fs::File::open(input).map_err(|e| {
             anyhow!("Failure to open input file {:?}: {:?}", input, e)
         })?;
-        for (ln, line_result) in std::io::BufReader::new(file)
+        for (line_num, line_result) in std::io::BufReader::new(file)
             .lines()
             .enumerate()
             .map(|(i, l)| (i + 1, l))
         {
-            let line = line_result?;
-            eprintln!("{}: {:?}", ln, line);
+            let line: String = line_result?;
+            eprintln!("{line_num}: {line:?}");
         }
         Ok(Self {})
     }
 
-    pub fn solve1(&self) -> Result<u64> {
+    pub fn solve1(&self) -> anyhow::Result<u64> {
         todo!();
     }
 
-    pub fn solve2(&self) -> Result<u64> {
+    pub fn solve2(&self) -> anyhow::Result<u64> {
         todo!();
     }
 }
